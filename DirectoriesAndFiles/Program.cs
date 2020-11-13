7e7823063885771e07555b6b54a148f72da3d777
Julia Aasmaa <my_e_10@hotmail.com>
2020-11-13 08:36:38 +0200

    commit

---
+++ DirectoriesAndFiles/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace DirectoriesAndFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootPath = @"C:\Users\julia.voronetskaja\samples";
+
+            //get the full path of all the direcories at the rootPath
+            string[] allDirectories = Directory.GetDirectories(rootPath);
+            //ReadArray(allDirectories);
+
+            //get nested directories
+            string[] includeNestedDirectories = Directory.GetDirectories(rootPath, "*", SearchOption.AllDirectories);
+            // ReadArray(includeNestedDirectories);
+
+
+            //get directory files
+            string[] files = Directory.GetFiles(rootPath, "*.*", SearchOption.AllDirectories);
+            ReadArray(files);
+
+
+
+        }
+
+        public static void ReadArray(string[] array)
+        {
+            foreach(string line in array)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
---
+++ WriteFilesAndDirectories/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace WriteFilesAndDirectories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteFilePaths();
+        }
+
+        public static void WriteFilePaths()
+        {
+            string filePath = @"C:\Users\julia.voronetskaja\samples\files.txt";
+            string directoryPath = @"C:\Users\julia.voronetskaja\samples";
+
+            string[] files = Directory.GetFiles(directoryPath, "*.*", SearchOption.AllDirectories);
+
+            //save to file
+            File.WriteAllLines(filePath, files);
+        }
+    }
+}
